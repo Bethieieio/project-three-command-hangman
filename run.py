d5ad0be5574164a8df1b35e0197c5a8a12029dd5
Beth Allaway <beth_allaway@yahoo.com>
2022-09-27 14:13:34 +0100

    create word list and add function to get random word to start game

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+#fetches random word from words.py and returns it in capital letters
+def = get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def 
