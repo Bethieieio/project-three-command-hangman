9ebba4443c3fca5cc95d9800654bad54cd79b1f0
Beth Allaway <beth_allaway@yahoo.com>
2022-09-28 22:14:45 +0100

    add main function, asking if player wants to play again

--- run.py
+++ run.py
@@ -57,4 +57,89 @@ def play(word):
     if guessed:
         print("Wahoo! You guessed the word! You Win!")
     else: 
-        print("I'm sorry, you ran our of tries. The correct word was " + word + "Try again!")
+        print("I'm sorry, you ran our of tries. The correct word was " + word + "Maybe next time!")
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \.
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? Y/N ").upper() == "Y"
+    word = get_word()
+    play(word)
+
+if __name__ == "__main__":
+    main()
